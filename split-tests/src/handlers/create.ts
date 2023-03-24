ec54793e948e3ccb8957a3e12abfc338b94a5f86
Timothy Dinkins <tim@dinkins.me>
2023-03-24 11:02:58 -0500

    Finished refactoring to TS

--- split-tests/src/handlers/create.ts
+++ split-tests/src/handlers/create.ts
@@ -2,11 +2,12 @@ import * as store from "../lib/stores/splitTestStore";
 import { getSplitTestService } from "../lib/services/splitTest";
 import { errorResponse, response } from "../lib/transport/http";
 import { SplitTest } from "../lib/types";
+import { APIGatewayProxyEvent, APIGatewayProxyResult } from "aws-lambda";
 
 const splitTestService = getSplitTestService(store);
 
-export const handler = async (event) => {
-  const params: SplitTest = JSON.parse(event.body);
+export const handler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+  const params: SplitTest = JSON.parse(event.body || "");
 
   try {
     const st: SplitTest = await splitTestService.create(params);
--- split-tests/src/handlers/split.ts
+++ split-tests/src/handlers/split.ts
@@ -2,14 +2,15 @@ import * as store from "../lib/stores/splitTestStore";
 import { getSplitTestService } from "../lib/services/splitTest";
 import { errorResponse } from "../lib/transport/http";
 import { SplitTestTarget } from "../lib/types";
+import { APIGatewayProxyEvent, APIGatewayProxyResult } from "aws-lambda";
 
 const splitTestService = getSplitTestService(store);
 
-export const handler = async (event) => {
+export const handler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
   const params = event.pathParameters;
 
   try {
-    if (!params.id) {
+    if (!params?.id) {
       throw new Error("No id");
     }
     const target: SplitTestTarget | undefined = await splitTestService.split(params.id);
@@ -17,15 +18,16 @@ export const handler = async (event) => {
       return {
         statusCode: 301,
         headers: {
-          Location: target,
+          Location: target.target,
         },
+        body: ""
       };
     } else {
-      return { statusCode: 404 };
+      return { body: "page not found", statusCode: 404 };
     }
   } catch (err) {
     return errorResponse(
-      `Error handling split with id: ${params.id}. Error: ${err.message}`,
+      `Error handling split with id: ${params?.id}. Error: ${err.message}`,
       err
     );
   }
